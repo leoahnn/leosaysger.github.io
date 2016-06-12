adb06100088e017f2f1aa59d143ae8a401cfcc69
Leo Ahnn <leoahnn@gmail.com>
2016-06-11 21:05:04 -0400

    fixed cursor

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -397,7 +397,7 @@
         // show cursor
         showCursor: true,
         // character for cursor
-        cursorChar: "▯",
+        cursorChar: "█",
         // attribute to type (null == text)
         attr: null,
         // either html or text
